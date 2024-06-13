52d7e0dc1f3133a56928aefb152807726eab9f00
nstraus7 <nstraus7@login01.cm.cluster>
2024-06-13 12:58:47 -0400

    cleaned, reverting to use mtex for preprocessingsing data

--- nest3d/data/old/ang2ctf/test_pymicro.py
+++ nest3d/data/old/ang2ctf/test_pymicro.py
@@ -1,9 +1,10 @@
 import pymicro
 from pymicro.crystal.ebsd import OimScan
+import os
 
 if __name__ == "__main__":
     file_path = "./A01_XY_100_nm.ang"
-
+    import pdb;pdb.set_trace()
     ebsd = OimScan.from_file(file_path)
 
     import pdb;pdb.set_trace()
